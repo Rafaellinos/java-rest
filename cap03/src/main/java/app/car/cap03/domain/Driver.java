ef27cd901601ca300d1369cb4804ef3d6fc85e11
Rafael Lino <rafael.veloso.lino@hotmail.com>
2021-03-17 08:49:51 -0300

    [ADD] cap03 and passengers api

---
+++ cap03/src/main/java/app/car/cap03/domain/Driver.java
@@ -0,0 +1,40 @@
+package app.car.cap03.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import java.util.Date;
+
+@Entity
+public class Driver {
+
+    @Id
+    @GeneratedValue
+    Long id;
+    String name;
+    Date BirthDate;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Date getBirthDate() {
+        return BirthDate;
+    }
+
+    public void setBirthDate(Date birthDate) {
+        BirthDate = birthDate;
+    }
+}
